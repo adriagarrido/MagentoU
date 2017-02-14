90284c766fd3ef9d83efc734d1e7cd61a6507978
Adrià Garrido <adriagarrido@gmail.com>
2017-02-14 08:27:14 +0100

    1.3.1
    Create a new module. Make a mistake in its config. Create a sencond
    module dependent on the first.

---
+++ app/code/Unit1/FirstModule/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ *
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Unit1_FirstModule',
+    __DIR__
+);
---
+++ app/code/Unit1/SecondModule/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ *
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Unit1_SecondModule',
+    __DIR__
+);
