f53429950123000cb553e5b022d26e885551dda1
Adrià Garrido <adriagarrido@gmail.com>
2017-03-09 14:45:41 +0100

    2.3.2
    Create a new router which "understands" URLs like
    /frontName-actionPath-action and converts them to
    /frontName/actionPath/action

---
+++ app/code/Training/Test/Controller/Router.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Training\Test\Controller;
+
+class Router implements \Magento\Framework\App\RouterInterface
+{
+    protected $actionFactory;
+
+    public function __construct(\Magento\Framework\App\ActionFactory $actionFactory)
+    {
+        $this->actionFactory = $actionFactory;
+    }
+
+    public function match(\Magento\Framework\App\RequestInterface $request)
+    {
+        $info = $request->getPathInfo();
+
+        // if (preg_match("%^/(test)-(.*?)-(.*?)$%", $info, $m)) {
+            $request->setPathInfo(sprintf("/%s/%s/%s", $m[1], $m[2], $m[3]));
+        return $this->actionFactory->create('Magento\Framework\App\Action\Forward', ['request' => $request]);
+        // }
+        return null;
+    }
+}
