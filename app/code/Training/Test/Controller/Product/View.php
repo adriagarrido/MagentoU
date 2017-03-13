3e6fbc263431333bcb82635ce8d7ca506c26d41a
Adrià Garrido <adriagarrido@gmail.com>
2017-03-13 11:00:34 +0100

    2.5.2
    Customize the catalog product view controller using plugins and
    preferences

---
+++ app/code/Training/Test/Controller/Product/View.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ *
+ */
+namespace Training\Test\Controller\Product;
+
+class View extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+        // echo "ONE";
+        // exit;
+    }
+
+    // public function beforeExecute()
+    // {
+    //     echo "BEFORE<BR />";
+    //     exit;
+    // }
+
+    // public function afterExecute(
+    //     \Magento\Catalog\Controller\Product\View $controller,
+    //     $result
+    // ) {
+    //     echo "AFTER";
+    //     exit;
+    // }
+}
