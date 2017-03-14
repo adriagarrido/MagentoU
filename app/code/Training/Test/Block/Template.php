1fef9d7f70b621d9ee1ecc98f8aed353b6289919
Adrià Garrido <adriagarrido@gmail.com>
2017-03-14 14:12:36 +0100

    3.6.2
    Create a template block and a custom template file for it. Render the
    block in the controller.

---
+++ app/code/Training/Test/Block/Template.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Training\Test\Block;
+
+class Template extends \Magento\Framework\View\Element\Template
+{
+}
--- app/code/Training/Test/Controller/Action/Index.php
+++ app/code/Training/Test/Controller/Action/Index.php
@@ -1,13 +1,13 @@
 <?php
-/**
- *
- */
 namespace Training\Test\Controller\Action;
 
 class Index extends \Magento\Framework\App\Action\Action
 {
     public function execute()
     {
-        $this->getResponse()->appendBody("HELLO WORLD");
+        // $this->getResponse()->appendBody("HELLO WORLD");
+        $block = $this->_view->getLayout()->createBlock('Training\Test\Block\Template');
+        $block->setTemplate('Training_Test::test.phtml');
+        $this->getResponse()->appendBody($block->toHtml());
     }
 }
